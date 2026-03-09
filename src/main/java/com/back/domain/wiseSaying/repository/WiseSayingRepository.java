e982b18c304929f7a4b02123d1e3be79464b9444
mk-404lab <alsgur9984@naver.com>
2026-03-10 00:56:25 +0900

    refactor: 24강 - JpaRepository를 상속 받아 WiseSayingRepository 재구현

--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -1,42 +1,10 @@
 package com.back.domain.wiseSaying.repository;
 
 import com.back.domain.wiseSaying.entity.WiseSaying;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
-@Repository
-public class WiseSayingRepository {
+public interface WiseSayingRepository extends JpaRepository<WiseSaying, Integer> {
 
-    private List<WiseSaying> wiseSayingList = new ArrayList<>();
-    int lastId = 0;
 
-    public WiseSaying save(WiseSaying wiseSaying) {
-        if (wiseSaying.isNew()) {
-            wiseSaying.setId(++lastId);
-        }
-        wiseSayingList.add(wiseSaying);
-
-        return wiseSaying;
-    }
-
-    public List<WiseSaying> findAll() {
-        return wiseSayingList;
-    }
-
-    public Optional<WiseSaying> findById(int id) {
-        return wiseSayingList.stream()
-                .filter(w -> w.getId() == id)
-                .findFirst();
-    }
-
-    public void delete(WiseSaying wiseSaying) {
-        wiseSayingList.remove(wiseSaying);
-    }
-
-    public long count() {
-        return wiseSayingList.size();
-    }
 }
